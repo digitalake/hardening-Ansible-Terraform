a10cae53deb9389e24fa291b342a553c7b075e05
Ivan <ivan.yakimenko.win@gmail.com>
2022-09-18 23:57:13 +0300

    disk 1 resized

--- libvirt.tf
+++ libvirt.tf
@@ -9,10 +9,10 @@ resource "libvirt_volume" "ubuntu-22-04-amd64" {
 }
 
 resource "libvirt_volume" "disk1" {
-  name           = "disk1"
-  base_volume_id = libvirt_volume.ubuntu-22-04-amd64.id
-  base_volume_pool           = "libvirt-vms"
-  size = 10*1000*1000*1000
+  name             = "disk1"
+  base_volume_id   = libvirt_volume.ubuntu-22-04-amd64.id
+  base_volume_pool = "libvirt-vms"
+  size             = 13221225472
 }
 
 
@@ -21,20 +21,20 @@ data "template_file" "user_data" {
 }
 
 resource "libvirt_cloudinit_disk" "commoninit" {
-  name = "commoninit.iso"
-  pool = "default" # List storage pools using virsh pool-list
-  user_data      = data.template_file.user_data.rendered
+  name      = "commoninit.iso"
+  pool      = "default" # List storage pools using virsh pool-list
+  user_data = data.template_file.user_data.rendered
 }
 
 
 # Define KVM domain to create
 resource "libvirt_domain" "ubuntu-jammy" {
-  name   = "ubuntu-jammy"
-  memory = "2048"
-  vcpu   = 2
-
+  name       = "ubuntu-jammy"
+  memory     = 2048
+  vcpu       = 2
+  qemu_agent = true
   network_interface {
-    network_name = "default" # List networks with virsh net-list
+    network_name   = "default" # List networks with virsh net-list
     wait_for_lease = true
   }
 
@@ -45,19 +45,19 @@ resource "libvirt_domain" "ubuntu-jammy" {
   cloudinit = libvirt_cloudinit_disk.commoninit.id
 
   console {
-    type = "pty"
+    type        = "pty"
     target_type = "serial"
     target_port = "0"
   }
 
   graphics {
-    type = "spice"
+    type        = "spice"
     listen_type = "address"
-    autoport = true
+    autoport    = true
   }
 }
 
 # Output Server IP
 output "ip" {
-  value = libvirt_domain.ubuntu-jammy.network_interface.0.addresses.0
+  value = libvirt_domain.ubuntu-jammy
 }
