a1c947a207cee06faee2ef34011d4416c66edd47
Ivan <ivan.yakimenko.win@gmail.com>
2022-09-21 13:18:39 +0300

    vms.tf deleted, inner content moved to variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,16 @@
-#variable "memoryMB" { default = 1024 * 2 }
-#
-#variable "cpu" { default = 2 }
-
-
 variable "vms" {
   description = "Map of vm names to configuration."
   type        = map(any)
 }
+
+#locals {
+#  virtual_machines = toset([
+#    "vm1",
+#    "vm2",
+#    "vm3"
+#  ])
+#}
+
+#variable "memoryMB" { default = 1024 * 2 }
+#
+#variable "cpu" { default = 2 }
--- vms.tf
+++
@@ -1,8 +0,0 @@
-#locals {
-#  virtual_machines = toset([
-#    "vm1",
-#    "vm2",
-#    "vm3"
-#  ])
-#}
-
