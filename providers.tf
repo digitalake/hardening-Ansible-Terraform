b65e7b90e2b6aa6f604015fe89c0d9de31caebc2
Ivan <ivan.yakimenko.win@gmail.com>
2022-09-29 13:29:08 +0300

    added .gitignore

--- providers.tf
+++ providers.tf
@@ -1,10 +1,26 @@
+###providers.tf###
+
+# Defining Terraform backend + required providers
 terraform {
+  backend "s3" {
+    bucket = "tfe"
+    key = "terraform.tfstate"
+    endpoint = "http://172.17.0.2:9000"
+    access_key="5Scp5tcOIaKYuyup"
+    secret_key="fW8XryLfvSPXmatcLUhXlu9c14qfqo7T"
+    region = "main"
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+    skip_region_validation = true
+    force_path_style = true
+  }
   required_providers {
     libvirt = {
       source = "dmacvicar/libvirt"
     }
   }
 }
+
 
 provider "libvirt" {
   ## Configuration options
